83f5f4c3c396e5bb21555c7e4e47a80e001036f2
MrShotDead <mrshotdead@gmail.com>
2024-10-13 16:56:54 +0100

    Add files via upload

---
+++ sogmalogma.py
@@ -0,0 +1,27 @@
+
+not_a_file = open("not_txt.txt","r")
+print (not_a_file.read())
+#not_a_file = open("not_txt.txt","w")
+#not_a_file.write("Plethora")
+#not_a_file.close()
+not_a_file = open("not_txt.txt","a")
+not_a_file.write(" not a quote")
+not_a_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#The Baldest Teacher
+#Hello in this story i will be telling you a legend about a teacher who was so bald she pulled a ligma
+#Once upon a time Sheeka
